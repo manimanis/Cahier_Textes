8465558a31d9ea042bc6ee1e9b0c6aa3ba551664
Mohamed Anis MANI <manimohamed@gmail.com>
2025-10-02 18:18:02 +0100

    Mises à jour Ajout du calendrier annuel 2SC1 + 2TI1 + 3T1

--- scripts/app3.js
+++ scripts/app3.js
@@ -18,9 +18,40 @@ const app = new Vue({
     enseignant: enseignant,
     seances: {},
     selectedClasse: "",
-    emploi_tab: []
+    emploi_tab: [],
+    monthes: [],
+    monthStartingDay: 7
   },
   mounted: function () {
+    const anneeScolaire = +annee_scolaire.substring(0, 4);
+    let year = anneeScolaire, month = 9;
+    for (let i = 0; i < 9; i++) {
+      const monthObj = this.getMonth(year, month);
+      if (monthObj.days[0].date.getDay() < this.monthStartingDay) {
+        this.monthStartingDay = monthObj.days[0].date.getDay();
+      }
+      this.monthes.push(monthObj);
+      month++;
+      if (month > 12) {
+        month = 1;
+        year++;
+      }
+    }
+    
+    classes.forEach(classe => {
+      this.loadClasse(classe)
+        .then(data => {
+          if (data == null) {
+            return null;
+          }
+          data.forEach(seance => {
+            const dt = new Date(seance.date);
+            const numMonth = (dt.getMonth() - 8) + (dt.getFullYear() - anneeScolaire) * 12;
+            this.monthes[numMonth].days[dt.getDate() - 1].obs += seance.classe + "<br>";
+          });
+        });
+    });
+    this.$forceUpdate();
   },
   methods: {
     /**
@@ -42,6 +73,29 @@ const app = new Vue({
         this.addAlertMessage('danger', error);
       }
     },
+    loadClasse: function (classe) {
+      return fetch(`json/${classe}.json`, {
+        method: "GET"
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data == null) {
+            return null;
+          }
+          const seances = data.map(s => new Seance(s));
+          seances.sort((a, b) => {
+            if (a.date > b.date) return 1;
+            if (a.date < b.date) return -1;
+            return 0;
+          });
+          seances.forEach((seance, idxSeance) => seance.index = (idxSeance + 1));
+          seances.reverse();
+          return seances;
+        })
+        .catch(error => {
+          this.addAlertMessage('danger', `Erreur lors du chargement des données pour ${classe}!`);
+        });
+    },
     loadData: function (classe) {
       if (!classe) {
         return;
@@ -49,26 +103,12 @@ const app = new Vue({
 
       this.seances[classe] = [];
 
-      return fetch(`json/${classe}.json`, {
-        method: "GET"
-      })
-        .then(response => response.json())
-        //.then(this.handleFetch)
-        .catch(error => {
-          this.addAlertMessage('danger', `Erreur lors du chargement des données pour ${classe}!`);
-        })
+      return this.loadClasse(classe)
         .then(data => {
           if (data == null) {
             return null;
           }
-          this.seances[classe] = data.map(s => new Seance(s));
-          this.seances[classe].sort((a, b) => {
-            if (a.date > b.date) return 1;
-            if (a.date < b.date) return -1;
-            return 0;
-          });
-          this.seances[classe].forEach((seance, idxSeance) => seance.index = (idxSeance + 1));
-          this.seances[classe].reverse();
+          this.seances[classe] = data;
           this.$forceUpdate();
         });
     },
@@ -77,5 +117,24 @@ const app = new Vue({
       this.selectedClasse = classe;
       this.loadData(classe);
     },
+    getMonth: function (year, month) {
+      const debMois = new Date(year, month - 1, 1);
+      const finMois = new Date(year + ((month == 12) ? 1 : 0), (month == 12) ? 0 : month, 1);
+      let currTime = debMois.getTime();
+      const thisMonth = {
+        "name": debMois.toLocaleString('fr-FR', { month: 'long' }) + " " + year,
+        "days": []
+      };
+      while (currTime < finMois.getTime()) {
+        const date = new Date(currTime);
+        currTime += 3600 * 24 * 1000;
+        thisMonth.days.push({
+          "date": date,
+          "dayName": date.toLocaleString('fr-FR', { weekday: 'long' }),
+          "obs": ""
+        });
+      }
+      return thisMonth;
+    }
   }
 });
