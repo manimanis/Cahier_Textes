3d5ec4e491c3f5d196a9d7fcd650c39814d02b97
Mohamed Anis MANI <manimohamed@gmail.com>
2026-09-17 12:15:30 +0100

    feat: add configuration and class entries for the 2026/2027 academic year

--- config.inc.php
+++ config.inc.php
@@ -10,4 +10,4 @@ define('BASE', 'fiches_rens');
 define('SERVER_NAME', 'manimanis.github.io');
 // Pour générer une nouvelle clé :
 // php -r "echo base64_encode(random_bytes(64));"
-define('SECRET_KEY', 'f8a2c9d4e1b7f3a6c8d0e2f4b6a8c0d2e4f6a8b0c2d4e6f8a0b2c4d6e8f0a2b4c6d8e0f2a4c6d8e0f2a4c6d8e0f2a4');
+define('SECRET_KEY', 'YWRtaW4=');
